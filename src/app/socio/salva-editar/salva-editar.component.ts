6fc6807b47cee587693be3c4ffbdeef68e067433
Josemalyson Oliveira <josemalyson@gmail.com>
2018-01-14 01:08:42 -0300

    fix error valitation form

--- src/app/socio/salva-editar/salva-editar.component.ts
+++ src/app/socio/salva-editar/salva-editar.component.ts
@@ -1,10 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
-
 import { SocioService } from '../socio.service';
-import { Socio } from '../../socio/socio';
-
 
 @Component({
   selector: 'app-salva-editar',
@@ -17,21 +14,20 @@ export class SalvaEditarComponent implements OnInit {
     private _socioService: SocioService,
     private formBuilder: FormBuilder) { }
 
-  public socio: Socio;
   formularioSocio: FormGroup;
 
   ngOnInit() {
-    this.socio = new Socio();
     this.formularioSocio = this.formBuilder.group({
       nome: [null, Validators.required],
-      sobreNome: [null, Validators.required, Validators.email],
+      sobreNome: [null, Validators.required],
       cpf: [null, Validators.required],
       email: [null, Validators.required]
     });
   }
 
-  salvarSocio() {
-    this._socioService.salvar(this.socio);
+  onSubmit() {
+    console.log(this.formularioSocio);
+    this._socioService.salvar(null);
   }
 
 }
